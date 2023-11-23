1fc8b2c49f11452e79ce26f8a1d30e9cf9da873b
sftekin <sftekin25@gmail.com>
2023-11-23 18:06:46 -0500

    project 2

--- helper.py
+++ helper.py
@@ -1,6 +1,14 @@
 import os
 
 
+def read_all_prompts(code_dir, file_dict):
+    for project_idx in os.listdir(code_dir):
+        prompt_path = f"{code_dir}/{project_idx}/prompt"
+        if os.path.exists(prompt_path):
+            with open(prompt_path, "r") as f:
+                file_dict[project_idx] = f.read()
+
+
 def read_coding_lib(code_dir, file_dict):
     for file_name in os.listdir(code_dir):
         file_path = f"{code_dir}/{file_name}"
--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import evadb
 import subprocess
 import pandas as pd
-from helper import read_coding_lib
+from helper import read_coding_lib, read_all_prompts
+from transformers import pipeline
+
+# from dotenv import load_dotenv
+
 
 DEFAULT_PROMPT_PATH = "default_prompts/password_generator/prompt"
 os.environ["OPENAI_API_KEY"] = \
-    os.environ.get("OPENAI_API_KEY", "sk-111111111111111111111111111111111111")
+    os.environ.get("OPENAI_API_KEY", "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")
 os.environ["OPENAI_KEY"] = os.environ["OPENAI_API_KEY"]
 
 
 def receive_prompt():
     # we will return the prompt str
-    prompt = ""
-
     print("Welcome to EvaDB, you can create any coding project on your local. "
           "\nYou only need to supply a prompt describing your coding project"
           " and an OpenAI API key.")
@@ -69,94 +71,135 @@ def summarize_project(cursor):
     print(summary)
 
 
-def generate_code(prompt, cursor):
-    # we will store the codes inside a temp folder
-    project_path = f"{os.getcwd()}/temp"
-    if not os.path.exists(project_path):
+class CodeGenerator:
+    def __init__(self, reset=True):
+        # we will store the codes inside a temp folder
+        self.project_dir = f"{os.getcwd()}/temp"
+        self.cursor = evadb.connect().cursor()
+        if reset:
+            self.cursor.query("DROP TABLE IF EXISTS prompts;").execute()
+
+        self.db_connected = False
+
+        # load prompt similarity checker
+        self.create_prompts_table()
+
+        self.cursor.query(
+            "CREATE FUNCTION IF NOT EXISTS CodeGPT IMPL 'code_gpt.py';"
+        ).execute()
+
+        self.pipe = pipeline("text-classification", model="AMHR/adversarial-paraphrasing-detector")
+
+    def create_prompts_table(self):
+        # self.cursor.query("DROP TABLE IF EXISTS prompts;").execute()
+        self.cursor.query(
+            """CREATE TABLE  IF NOT EXISTS prompts (project_id INTEGER, prompt TEXT(200));"""
+        ).execute()
+        prompts = {}
+        read_all_prompts(code_dir=self.project_dir, file_dict=prompts)
+        if prompts:
+            for project, prompt in prompts.items():
+                project_id = int(project.split("-")[1])
+                query = f"INSERT INTO prompts (project_id, prompt) VALUES ({project_id}, \"({prompt})\");"
+                self.cursor.query(query).execute()
+
+    def check_prompt_sim(self, input_prompt):
+        table = self.cursor.query("SELECT project_id, prompt FROM prompts;").execute().frames
+
+        comp_prompts = []
+        for i, row in table.iterrows():
+            row_prompt = row["prompt"].replace("(", "").replace(")", "")
+            compare_prompt = ". ".join([row_prompt.split(".")[0], input_prompt.split(".")[0]])
+            comp_prompts.append(compare_prompt)
+        response = self.pipe(comp_prompts)
+        found_idx = [i + 1 for i, r in enumerate(response) if r["label"] == "LABEL_1"]
+
+        completed_idx = []
+        for i in found_idx:
+            all_output_path = f"{self.project_dir}/project-{i}/workspace/all_output.txt"
+            if os.path.exists(all_output_path):
+                completed_idx.append(i)
+
+        if any(completed_idx):
+            return True, table.iloc[completed_idx[0]]["project_id"]
+        else:
+            return False, None
+
+    def generate(self, prompt):
+        exists_flag, project_id = self.check_prompt_sim(input_prompt=prompt)
+        if exists_flag:
+            all_output_path = f"{self.project_dir}/project-{project_id}/workspace/all_output.txt"
+            with open(all_output_path, "r") as f:
+                all_output = f.read()
+            print(all_output)
+
+            answer = ""
+            while answer not in ["y", "n"]:
+                answer = input("Would you like to run the code y/n ?")
+
+            if answer == "y":
+                self.execute_code(project_id)
+            else:
+                return
+
+        project_path = self.get_project_path()
+        prompt_path = f"{project_path}/prompt"
+        with open(prompt_path, "w") as f:
+            f.write(prompt)
+
+        # generate the code by calling CodeGPT functionality
+        query = f"SELECT CodeGPT(\"{project_path}\", \"\");"
+        self.cursor.query(query).df()
+
+    def execute_code(self, project_id):
+        project_path = f"{self.project_dir}/project-{project_id}"
+
+        print(f"Executing the project-{project_id} inside {project_path}")
+        print("-*-*" * 30)
+        os.chdir(f"temp/project-{project_id}/workspace/")
+        command = ["chmod", "+x", "run.sh"]
+        proc = subprocess.run(command, text=True)
+
+        command = [f"./run.sh"]
+        proc = subprocess.run(command, text=True)
+
+    def get_project_path(self):
+        if not os.path.exists(self.project_dir):
+            os.makedirs(self.project_dir)
+            file_index = 1
+        elif len(os.listdir(self.project_dir)) == 0:
+            file_index = 1
+        else:
+            # temp exits get the file_index
+            file_indices = [int(file_name.split("-")[1])
+                            for file_name in os.listdir(self.project_dir)]
+            file_index = sorted(file_indices)[-1] + 1
+
+        # write the prompt to a file in the temp directory
+        project_path = f"{self.project_dir}/project-{file_index}"
         os.makedirs(project_path)
-        file_index = 1
-    else:
-        # temp exits get the file_index
-        file_indices = [int(file_name.split("-")[1])
-                        for file_name in os.listdir(project_path)]
-        file_index = sorted(file_indices)[-1] + 1
 
-    # write the prompt to a file in the temp directory
-    project_path = f"{project_path}/project-{file_index}"
-    os.makedirs(project_path)
-    prompt_path = f"{project_path}/prompt"
-    with open(prompt_path, "w") as f:
-        f.write(prompt)
-
-    # generate the code by calling gpt-engineer
-    print(f"Creating the project-{file_index} inside {project_path}")
-    print("-*-*" * 30)
-    command = ["gpt-engineer", project_path]
-    proc = subprocess.run(command, text=True)
-
-    # index the generated code into EvaDB
-    file_index = 2
-    project_path = f"{project_path}/project-{file_index}"
-    index_code(project_path, cursor)
-
-    return file_index
-
-
-def index_code(project_path, cursor):
-    project_id = int(os.path.basename(project_path).split("-")[1])
-    workspace_path = f"{project_path}/workspace"
-    code_contents = {}
-    read_coding_lib(code_dir=workspace_path, file_dict=code_contents)
-    data = {
-        "project_id": [project_id] * len(code_contents),
-        "file_names": list(code_contents.keys()),
-        "file_path": [project_path] * len(code_contents)
-    }
-    data_df = pd.DataFrame(data=data)
-    summary_df = pd.DataFrame([{"project_id": [project_id],
-                                "summary": code_contents["all_output.txt"]}])
-
-    cursor.query(
-        """CREATE TABLE IF NOT EXISTS Summary (project_id INTEGER, summary TEXT(1000));"""
-    ).execute()
-    summary_path = f"{project_path}/summary.csv"
-    summary_df.to_csv(summary_path)
-    cursor.load(summary_path, "Summary", "csv").execute()
-
-    cursor.query(
-        """CREATE TABLE IF NOT EXISTS Codes (project_id INTEGER, file_names TEXT(20), file_path TEXT(100));"""
-    ).execute()
-    data_path = f"{project_path}/data.csv"
-    data_df.to_csv(data_path)
-    cursor.load(data_path, "Codes", "csv").execute()
-
-
-
-def execute_code(project_id):
-    project_path = f"{os.getcwd()}/temp/project-{project_id}"
-
-    print(f"Executing the project-{project_id} inside {project_path}")
-    print("-*-*" * 30)
-    os.chdir(f"temp/project-{project_id}/workspace/")
-    command = ["chmod", "+x", "run.sh"]
-    proc = subprocess.run(command, text=True)
-
-    command = [f"./run.sh"]
-    proc = subprocess.run(command, text=True)
+        return project_path
 
 
 def run():
-    cursor = evadb.connect().cursor()
-    prompt = "Develop a Pomodoro timer app using HTML, CSS, " \
-             "and JavaScript. Allow users to set work and break" \
-             " intervals and receive notifications when it's time to switch."
+    code_generator = CodeGenerator()
+
+    # prompt = "Develop a Pomodoro timer app using HTML, CSS, " \
+    #          "and JavaScript. Allow users to set work and break" \
+    #          " intervals and receive notifications when it's time to switch."
+
+    prompt = "Create a simple to-do list app using HTML, CSS, and JavaScript." \
+             " Store tasks in local storage and allow users to add, edit, and delete tasks."
+
+    code_generator.generate(prompt)
 
     # interactive terminal asking for prompt
     # receive_prompt()
 
-    project_id = generate_code(prompt=prompt, cursor=cursor)
-
-    execute_code(project_id=project_id)
+    # project_id = generate_code(prompt=prompt, cursor=cursor)
+    #
+    # execute_code(project_id=project_id)
 
 
 if __name__ == '__main__':
